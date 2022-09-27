7a17272161d74e97c2f089c7e93a8598c193af83
Rodesar <rockoesp@hotmail.com>
2022-09-26 19:57:21 -0500

    Verify that password fields are not empty

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const firstName = document.querySelector('#first-name');
 const lastName = document.querySelector('#last-name');
 const email = document.querySelector('#email');
 const submitBtn = document.querySelector('button');
-const spanPassword = document.querySelector('.dont-match');
+const spanPassword = document.querySelector('.invalid-password');
 
 password.addEventListener('change', () => {
     if (confirmPassword.value === '') {
@@ -18,6 +18,7 @@ password.addEventListener('change', () => {
     if (password.value !== confirmPassword.value) {
         password.classList.add('error');
         password.setCustomValidity('Passwords do not match');
+        spanPassword.setAttribute('invalid-text', 'Passwords do not match');
         confirmPassword.classList.add('error');
     } else {
         password.classList.remove('error');
@@ -30,7 +31,12 @@ confirmPassword.addEventListener('change', () => {
     if (password.value !== confirmPassword.value) {
         password.classList.add('error');
         password.setCustomValidity('Passwords do not match');
+        spanPassword.setAttribute('invalid-text', 'Passwords do not match');
         confirmPassword.classList.add('error');
+    } else if (password.value === '') {
+        password.classList.add('error');
+        password.setCustomValidity('Create a password');
+        spanPassword.setAttribute('invalid-text', 'Create a password');
     } else {
         password.classList.remove('error');
         password.setCustomValidity('');
@@ -41,29 +47,44 @@ confirmPassword.addEventListener('change', () => {
 firstName.addEventListener('focusout', () => {
     if (firstName.value === '') {
         firstName.classList.add('error');
-        firstName.setCustomValidity('What\'s your name')
+        firstName.setCustomValidity('What\'s your name');
     } else {
         firstName.classList.remove('error');
-        firstName.setCustomValidity('')
+        firstName.setCustomValidity('');
     }
 });
 
 lastName.addEventListener('focusout', () => {
     if (lastName.value === '') {
         lastName.classList.add('error');
-        lastName.setCustomValidity('What\'s your name')
+        lastName.setCustomValidity('What\'s your name');
     } else {
         lastName.classList.remove('error');
-        lastName.setCustomValidity('')
+        lastName.setCustomValidity('');
     }
 });
 
 email.addEventListener('focusout', () => {
     if (email.value === '') {
         email.classList.add('error');
-        email.setCustomValidity('You\'ll use this to log in')
+        email.setCustomValidity('You\'ll use this to log in');
     } else {
         email.classList.remove('error');
-        email.setCustomValidity('')
+        email.setCustomValidity('');
+    }
+});
+
+password.addEventListener('focusout', () => {
+    if (password.value === '') {
+        password.classList.add('error');
+        password.setCustomValidity('Create a password');
+        spanPassword.setAttribute('invalid-text', 'Create a password');
+    }
+});
+
+confirmPassword.addEventListener('focusout', () => {
+    if (confirmPassword.value === '') {
+        confirmPassword.classList.add('error');
+        confirmPassword.setCustomValidity('Confirm your password');
     }
 });
